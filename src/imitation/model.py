7d142b9b7e2ebdf91e2a9ec1041bfc5e6324d52d
Jon Goikoetxea <jon.goikoetxea.m@gmail.com>
2026-09-23 10:21:45 +0200

    Rename chunk_size to action_chunk_horizon

--- src/imitation/model.py
+++ src/imitation/model.py
@@ -15,13 +15,15 @@ class BasePolicyModel(nn.Module, metaclass=abc.ABCMeta):
 
     state_dim: int
     action_dim: int
-    chunk_size: int
+    action_chunk_horizon: int
 
-    def __init__(self, state_dim: int, action_dim: int, chunk_size: int) -> None:
+    def __init__(
+        self, state_dim: int, action_dim: int, action_chunk_horizon: int
+    ) -> None:
         super().__init__()
         self.state_dim = state_dim
         self.action_dim = action_dim
-        self.chunk_size = chunk_size
+        self.action_chunk_horizon = action_chunk_horizon
 
     @abc.abstractmethod
     def compute_loss(
@@ -59,11 +61,13 @@ class MSEPolicyModel(BasePolicyModel):
         self,
         state_dim: int,
         action_dim: int,
-        chunk_size: int,
+        action_chunk_horizon: int,
         hidden_dims: tuple[int, ...] = (128, 128),
     ) -> None:
-        super().__init__(state_dim, action_dim, chunk_size)
-        self.mlp = make_relu_mlp(state_dim, chunk_size * action_dim, hidden_dims)
+        super().__init__(state_dim, action_dim, action_chunk_horizon)
+        self.mlp = make_relu_mlp(
+            state_dim, action_chunk_horizon * action_dim, hidden_dims
+        )
 
     def compute_loss(
         self,
@@ -71,7 +75,7 @@ class MSEPolicyModel(BasePolicyModel):
         action_chunk: torch.Tensor,
     ) -> torch.Tensor:
         pred_action_chunk = rearrange(
-            self.mlp(state), "b (t a) -> b t a", t=self.chunk_size
+            self.mlp(state), "b (t a) -> b t a", t=self.action_chunk_horizon
         )
         batch_size = state.shape[0]
         loss = nn.functional.mse_loss(pred_action_chunk, action_chunk, reduction="sum")
@@ -85,7 +89,7 @@ class MSEPolicyModel(BasePolicyModel):
     ) -> torch.Tensor:
         with torch.no_grad():
             pred_action_chunk = rearrange(
-                self.mlp(state), "b (t a) -> b t a", t=self.chunk_size
+                self.mlp(state), "b (t a) -> b t a", t=self.action_chunk_horizon
             )
         return pred_action_chunk
 
@@ -100,9 +104,9 @@ class FlowMatchingPolicyModel(BasePolicyModel):
         vel_predictor: BaseVelocityPredictor,
         state_dim: int,
         action_dim: int,
-        chunk_size: int,
+        action_chunk_horizon: int,
     ) -> None:
-        super().__init__(state_dim, action_dim, chunk_size)
+        super().__init__(state_dim, action_dim, action_chunk_horizon)
         self.vel_predictor = vel_predictor
 
     def compute_loss(
@@ -131,9 +135,9 @@ class FlowMatchingPolicyModel(BasePolicyModel):
     ) -> torch.Tensor:
         batch_size = state.shape[0]
         device = state.device
-        action_chunk = torch.randn(batch_size, self.chunk_size, self.action_dim).to(
-            device
-        )
+        action_chunk = torch.randn(
+            batch_size, self.action_chunk_horizon, self.action_dim
+        ).to(device)
         with torch.no_grad():
             for time in torch.linspace(0, 1, num_steps + 1)[:-1].to(device):
                 pred_velocity = self.vel_predictor.predict_velocity(
@@ -178,7 +182,7 @@ def build_policy(
         return MSEPolicyModel(
             state_dim=state_dim,
             action_dim=action_dim,
-            chunk_size=chunk_size,
+            action_chunk_horizon=chunk_size,
             hidden_dims=hidden_dims,
         )
     if policy_config.architecture == "mlp":
@@ -202,5 +206,5 @@ def build_policy(
         predictor,
         state_dim=state_dim,
         action_dim=action_dim,
-        chunk_size=chunk_size,
+        action_chunk_horizon=chunk_size,
     )
